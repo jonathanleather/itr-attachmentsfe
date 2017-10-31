937b2d0a9899109c40c6591f6b92fd18e8524373
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2017-10-31 16:38:52 +0000

    UPGRADE

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -19,14 +19,8 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val playHealthVersion = "2.1.0"
-  private val logbackJsonLoggerVersion = "3.1.0"
   private val frontendBootstrapVersion = "8.9.0"
-  private val govukTemplateVersion = "5.1.0"
-  private val playUiVersion = "7.4.0"
   private val playPartialsVersion = "6.1.0"
-  private val playAuthorisedFrontendVersion = "6.4.0"
-  private val playConfigVersion = "4.3.0"
   private val hmrcTestVersion = "2.3.0"
   private val cachingClientVersion = "7.0.0"
   private val playConditionalMappingVersion = "0.2.0"
@@ -35,8 +29,7 @@ private object AppDependencies {
   private val pegDownVersion = "1.6.0"
   private val jSoupVersion = "1.8.3"
   private val mockitoAll = "1.9.5"
-  private val playGraphiteVersion = "3.2.0"
-
+  
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
